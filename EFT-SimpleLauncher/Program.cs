412f53f22415c4b87a4ef1d8437ae60232284fa9
Merijn Hendriks <innohurrytocode@gmail.com>
2019-11-20 18:15:03 +0100

    Initial project

---
+++ EFT-SimpleLauncher/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+public static class Program
+{
+	private static void Main(string[] args)
+	{
+		string filePath = Path.Combine(Environment.CurrentDirectory, "./EscapeFromTarkov.exe");
+		string launchArgs = "-bC5vLmcuaS5u=eyBlbWFpbDogInVzZXIwQGpldC5jb20iLCBwYXNzd29yZDogInBhc3N3b3JkIiwgdG9nZ2xlOiB0cnVlLCB0aW1lc3RhbXA6IDEzMjE3ODA5NzYzNTM2MTQ4M30= -token=0 -screenmode=fullscreen";
+
+		Process.Start(filePath, launchArgs);
+	}
+}
--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # EFT-SimpleLauncher
-Simple game launcher for Escape From Tarkov 0.12.xxxx to be used in conjunction with EmuTarkov 0.7.7
+Simple game launcher for Escape From Tarkov 0.12.xxxx.
+
+## Requirements
+- Visual Studio 2017
+- .NET Framework 2.0
+- Escape From Tarkov 0.12.xxxx
+
+## Installation
+1. Download the latest release
+2. Copy-paste "GameLauncher.exe" to Escape From Tarkov's directory
+
+## Usage
+1. Start the server emulator
+2. Run GameLauncher.exe
